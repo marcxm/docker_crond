28ee907ceeb2e55579ae23f423b26d75f39ebf75
marcxm <marc_smith@gmx.com>
2022-02-20 01:44:43 +0100

    Changes to be committed: 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-useradd -m $USERNAME &
+f [ "${USERNAME}" = "" ]; then
+  echo "No USERNAME given. NOT creating user account. Rely on root account."
+  exit
+else
+  echo "Variable is: $USERNAME. Creating user account."
+  useradd -m $USERNAME
+  exit
+fi &
 
 echo "root:$ROOTPASSWORD"|chpasswd &
 echo "$USERNAME:$USERPASSWORD"|chpasswd &

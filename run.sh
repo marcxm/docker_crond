ea3272099ab3ad3a803414bf8cb89648abd795ba
marcxm <marc_smith@gmx.com>
2022-02-20 01:53:53 +0100

    Changes to be committed:

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ if [ "${USERNAME}" = "" ]; then
 else
   echo "Variable is: $USERNAME. Creating user account."
   useradd -m $USERNAME
+  echo "$USERNAME:$USERPASSWORD"|chpasswd
   exit
 fi &
 
 echo "root:$ROOTPASSWORD"|chpasswd &
-echo "$USERNAME:$USERPASSWORD"|chpasswd &
 
 /usr/bin/crond -n
